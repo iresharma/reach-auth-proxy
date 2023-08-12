c1d825ccfb4088c46c2c1e7c87d1b7d3d2dc5e9a
iresharma <iresh.sharma8@gmail.com>
2023-08-12 21:17:12 +0530

    kanban gRPC client init

--- internal/pkg/RPC/kanban/main.go
+++ internal/pkg/RPC/kanban/main.go
@@ -2,11 +2,13 @@ package kanban
 
 import (
 	kanbanProto "awesomeProject/internal/pkg/RPC/kanban/proto"
-	types "awesomeProject/internal/pkg/types"
 	"context"
+	"encoding/json"
+	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
+	"net/url"
 	"time"
 )
 
@@ -23,19 +25,88 @@ func CreateKanbanClient() (kanbanProto.KanbanPackageClient, *grpc.ClientConn) {
 	return client, kanbanConn
 }
 
-func CreateKanban(messageInterface types.MessageInterface) kanbanProto.BoardResponse {
+func CreateKanban(userAccountId string) kanbanProto.BoardResponse {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	client, conn := CreateKanbanClient()
 	defer conn.Close()
 
 	reqObj := kanbanProto.CreateKanbanRequest{
-		UserAccountId: messageInterface.Headers["X-Useraccount"][0],
+		UserAccountId: userAccountId,
 	}
 
 	res, err := client.InitializeKanban(ctx, &reqObj)
 	if err != nil {
 		log.Fatalf("Error creating kanban board")
+	}
+	return *res
+}
+
+func AddLabel(boardId string, color string, label string) kanbanProto.Label {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	client, conn := CreateKanbanClient()
+	defer conn.Close()
+
+	reqObj := kanbanProto.LabelRequest{
+		Name:    label,
+		Color:   color,
+		BoardId: boardId,
+	}
+
+	res, err := client.AddLabel(ctx, &reqObj)
+	if err != nil {
+		log.Fatalf("Error creating a new label")
+	}
+	return *res
+}
+
+func AddItem(body url.Values, board string) kanbanProto.Item {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	client, conn := CreateKanbanClient()
+	defer conn.Close()
+
+	id := uuid.New()
+	var status kanbanProto.STATUS
+
+	switch body.Get("status") {
+	case "todo":
+		status = kanbanProto.STATUS_TODO
+		break
+	case "progress":
+		status = kanbanProto.STATUS_PROGRESS
+		break
+	case "backlog":
+		status = kanbanProto.STATUS_BACKLOG
+		break
+	case "completed":
+		status = kanbanProto.STATUS_COMPLETED
+		break
+	case "cancelled":
+		status = kanbanProto.STATUS_CANCELED
+		break
+	}
+
+	var links map[string]string
+	err := json.Unmarshal([]byte(body.Get("links")), &links)
+	if err != nil {
+		panic(err)
+	}
+
+	reqObj := kanbanProto.AddItemRequest{
+		Id:      id.String(),
+		Label:   body.Get("label"),
+		Status:  status,
+		Title:   body.Get("title"),
+		Desc:    body.Get("desc"),
+		Links:   links,
+		BoardId: board,
+	}
+
+	res, err := client.AddItem(ctx, &reqObj)
+	if err != nil {
+		log.Fatalf("Error creating a new label")
 	}
 	return *res
 }
--- internal/pkg/RPC/procedure_maps.go
+++ internal/pkg/RPC/procedure_maps.go
@@ -9,8 +9,20 @@ import (
 func ProceduresMapping(input types.MessageInterface) (map[string]interface{}, *types.Error) {
 	switch input.Name {
 	case "/kanban/create":
-		res := kanban.CreateKanban(input)
+		res := kanban.CreateKanban(input.Headers["X-Useraccount"][0])
 		return StructToMap(res), nil
+
+	case "/kanban/label/add":
+		label := input.Body["label"][0]
+		color := input.Body["color"][0]
+		boardId := input.Body["board"][0]
+		res := kanban.AddLabel(boardId, label, color)
+		return StructToMap(res), nil
+	case "/kanban/item/create":
+		board := input.Query["board"][0]
+		res := kanban.AddItem(input.Body, board)
+		return StructToMap(res), nil
+
 	default:
 		fmt.Println("everything")
 	}
