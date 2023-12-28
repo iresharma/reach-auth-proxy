9d1fd489d1373e77df167317ab96f76fdeaeddd8
iresharma <iresh.sharma8@gmail.com>
2023-12-29 02:22:31 +0530

    schema DDL and ER diagram

--- schema.sql
+++ schema.sql
@@ -1,299 +1,299 @@
-create table public.user_accounts
+create table if not exists user_accounts
 (
-id           text not null
-primary key,
-created_at   timestamp with time zone,
-updated_at   timestamp with time zone,
-deleted_at   timestamp with time zone,
-account_name text,
-email        text,
-photo_url    text,
-owner        text,
-page_id      text,
-bucket_id    text,
-board_id     text
+    id           text not null
+    primary key,
+    created_at   timestamp with time zone,
+    updated_at   timestamp with time zone,
+    deleted_at   timestamp with time zone,
+    account_name text,
+    email        text,
+    photo_url    text,
+    owner        text,
+    page_id      text,
+    bucket_id    text,
+    board_id     text
 );
 
-alter table public.user_accounts
-owner to iresharma;
+alter table user_accounts
+    owner to iresharma;
 
-create index idx_user_accounts_deleted_at
-on public.user_accounts (deleted_at);
+create index if not exists idx_user_accounts_deleted_at
+    on user_accounts (deleted_at);
 
-create table public.auths
+create table if not exists auths
 (
-id              text not null
-primary key,
-created_at      timestamp with time zone,
-updated_at      timestamp with time zone,
-deleted_at      timestamp with time zone,
-email           text
-unique,
-password_hash   text,
-salt            text,
-perm            text,
-user_account_id text
-constraint fk_user_accounts_users
-references public.user_accounts,
-metadata_id     text,
-settings_id     text
+    id              text not null
+    primary key,
+    created_at      timestamp with time zone,
+    updated_at      timestamp with time zone,
+    deleted_at      timestamp with time zone,
+    email           text
+    unique,
+    password_hash   text,
+    salt            text,
+    perm            text,
+    user_account_id text
+    constraint fk_user_accounts_users
+    references user_accounts,
+    metadata_id     text,
+    settings_id     text
 );
 
-alter table public.auths
-owner to iresharma;
+alter table auths
+    owner to iresharma;
 
-create index idx_auths_deleted_at
-on public.auths (deleted_at);
+create index if not exists idx_auths_deleted_at
+    on auths (deleted_at);
 
-create table public.sessions
+create table if not exists sessions
 (
-id         text not null
-primary key,
-created_at timestamp with time zone,
-updated_at timestamp with time zone,
-deleted_at timestamp with time zone,
-auth_id    text
-constraint fk_sessions_auth
-references public.auths
+    id         text not null
+    primary key,
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    deleted_at timestamp with time zone,
+    auth_id    text
+    constraint fk_sessions_auth
+    references auths
 );
 
-alter table public.sessions
-owner to iresharma;
+alter table sessions
+    owner to iresharma;
 
-create index idx_sessions_deleted_at
-on public.sessions (deleted_at);
+create index if not exists idx_sessions_deleted_at
+    on sessions (deleted_at);
 
-create table public.user_account_invite_codes
+create table if not exists user_account_invite_codes
 (
-id              text not null
-primary key,
-created_at      timestamp with time zone,
-updated_at      timestamp with time zone,
-deleted_at      timestamp with time zone,
-code            text,
-user_account_id text
+    id              text not null
+    primary key,
+    created_at      timestamp with time zone,
+    updated_at      timestamp with time zone,
+    deleted_at      timestamp with time zone,
+    code            text,
+    user_account_id text
 );
 
-alter table public.user_account_invite_codes
-owner to iresharma;
+alter table user_account_invite_codes
+    owner to iresharma;
 
-create index idx_user_account_invite_codes_deleted_at
-on public.user_account_invite_codes (deleted_at);
+create index if not exists idx_user_account_invite_codes_deleted_at
+    on user_account_invite_codes (deleted_at);
 
-create table public.board
+create table if not exists board
 (
-id varchar(255) not null
-primary key
+    id varchar(255) not null
+    primary key
+    );
+
+alter table board
+    owner to iresharma;
+
+create table if not exists kanbanlabel
+(
+    id           varchar(255) not null
+    primary key,
+    name         varchar(255) not null,
+    color        varchar(255) not null,
+    "boardId_id" varchar(255) not null
+    references board
+    );
+
+alter table kanbanlabel
+    owner to iresharma;
+
+create unique index if not exists kanbanlabel_name
+    on kanbanlabel (name);
+
+create index if not exists "kanbanlabel_boardId_id"
+    on kanbanlabel ("boardId_id");
+
+create table if not exists item
+(
+    id       varchar(255) not null
+    primary key,
+    status   varchar(255) not null,
+    title    varchar(255) not null,
+    "desc"   text         not null,
+    links    varchar(255) not null,
+    board_id varchar(255) not null
+    references board,
+    label_id varchar(255) not null
+    references kanbanlabel
+    );
+
+alter table item
+    owner to iresharma;
+
+create index if not exists item_board_id
+    on item (board_id);
+
+create index if not exists item_label_id
+    on item (label_id);
+
+create table if not exists comment
+(
+    id       varchar(255) not null
+    primary key,
+    "userId" varchar(255) not null,
+    message  text         not null,
+    item_id  varchar(255) not null
+    references item
+    );
+
+alter table comment
+    owner to iresharma;
+
+create index if not exists comment_item_id
+    on comment (item_id);
+
+create table if not exists reaction
+(
+    id         varchar(255) not null
+    primary key,
+    "userId"   varchar(255) not null,
+    emoji      varchar(255) not null,
+    comment_id varchar(255) not null
+    references comment
+    );
+
+alter table reaction
+    owner to iresharma;
+
+create index if not exists reaction_comment_id
+    on reaction (comment_id);
+
+create table if not exists pages
+(
+    id              text not null
+    primary key,
+    created_at      timestamp with time zone,
+    updated_at      timestamp with time zone,
+    deleted_at      timestamp with time zone,
+    route           text,
+    user_account_id text
 );
 
-alter table public.board
-owner to iresharma;
+alter table pages
+    owner to iresharma;
 
-create table public.kanbanlabel
+create index if not exists idx_pages_deleted_at
+    on pages (deleted_at);
+
+create table if not exists templates
 (
-id           varchar(255) not null
-primary key,
-name         varchar(255) not null,
-color        varchar(255) not null,
-"boardId_id" varchar(255) not null
-references public.board
+    id              text not null
+    primary key,
+    created_at      timestamp with time zone,
+    updated_at      timestamp with time zone,
+    deleted_at      timestamp with time zone,
+    name            text,
+    "desc"          text,
+    image           text,
+    button          text,
+    background      text,
+    font            text,
+    font_color      text,
+    page_id         text
+    constraint fk_pages_template
+    references pages,
+    social          boolean,
+    social_position text
 );
 
-alter table public.kanbanlabel
-owner to iresharma;
+alter table templates
+    owner to iresharma;
 
-create unique index kanbanlabel_name
-on public.kanbanlabel (name);
+create index if not exists idx_templates_deleted_at
+    on templates (deleted_at);
 
-create index "kanbanlabel_boardId_id"
-on public.kanbanlabel ("boardId_id");
-
-create table public.item
+create table if not exists meta
 (
-id       varchar(255) not null
-primary key,
-status   varchar(255) not null,
-title    varchar(255) not null,
-"desc"   text         not null,
-links    varchar(255) not null,
-board_id varchar(255) not null
-references public.board,
-label_id varchar(255) not null
-references public.kanbanlabel
+    id          text not null
+    primary key,
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone,
+    deleted_at  timestamp with time zone,
+    value       text,
+    type        text,
+    template_id text
+    constraint fk_templates_meta_tags
+    references templates
 );
 
-alter table public.item
-owner to iresharma;
+alter table meta
+    owner to iresharma;
 
-create index item_board_id
-on public.item (board_id);
+create index if not exists idx_meta_deleted_at
+    on meta (deleted_at);
 
-create index item_label_id
-on public.item (label_id);
-
-create table public.comment
+create table if not exists page_links
 (
-id       varchar(255) not null
-primary key,
-"userId" varchar(255) not null,
-message  text         not null,
-item_id  varchar(255) not null
-references public.item
+    id         text not null
+    primary key,
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    deleted_at timestamp with time zone,
+    name       text,
+    link       text,
+    page_id    text
+    constraint fk_pages_links
+    references pages,
+    icon       text,
+    social     boolean,
+    sequence   bigint
 );
 
-alter table public.comment
-owner to iresharma;
+alter table page_links
+    owner to iresharma;
 
-create index comment_item_id
-on public.comment (item_id);
+create index if not exists idx_page_links_deleted_at
+    on page_links (deleted_at);
 
-create table public.reaction
+create table if not exists maillist
 (
-id         varchar(255) not null
-primary key,
-"userId"   varchar(255) not null,
-emoji      varchar(255) not null,
-comment_id varchar(255) not null
-references public.comment
+    id         serial
+    primary key,
+    email      varchar(255) not null,
+    topics     varchar(255) not null,
+    created_at timestamp    not null
+    );
+
+alter table maillist
+    owner to iresharma;
+
+create unique index if not exists maillist_email
+    on maillist (email);
+
+create table if not exists settings
+(
+    id         text not null
+    primary key,
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    deleted_at timestamp with time zone
 );
 
-alter table public.reaction
-owner to iresharma;
+alter table settings
+    owner to iresharma;
 
-create index reaction_comment_id
-on public.reaction (comment_id);
+create index if not exists idx_settings_deleted_at
+    on settings (deleted_at);
 
-create table public.pages
+create table if not exists metadata
 (
-id              text not null
-primary key,
-created_at      timestamp with time zone,
-updated_at      timestamp with time zone,
-deleted_at      timestamp with time zone,
-route           text,
-user_account_id text
+    id         text not null
+    primary key,
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    deleted_at timestamp with time zone,
+    name       text,
+    photo_url  text
 );
 
-alter table public.pages
-owner to iresharma;
+alter table metadata
+    owner to iresharma;
 
-create index idx_pages_deleted_at
-on public.pages (deleted_at);
-
-create table public.templates
-(
-id              text not null
-primary key,
-created_at      timestamp with time zone,
-updated_at      timestamp with time zone,
-deleted_at      timestamp with time zone,
-name            text,
-"desc"          text,
-image           text,
-button          text,
-background      text,
-font            text,
-font_color      text,
-page_id         text
-constraint fk_pages_template
-references public.pages,
-social          boolean,
-social_position text
-);
-
-alter table public.templates
-owner to iresharma;
-
-create index idx_templates_deleted_at
-on public.templates (deleted_at);
-
-create table public.meta
-(
-id          text not null
-primary key,
-created_at  timestamp with time zone,
-updated_at  timestamp with time zone,
-deleted_at  timestamp with time zone,
-value       text,
-type        text,
-template_id text
-constraint fk_templates_meta_tags
-references public.templates
-);
-
-alter table public.meta
-owner to iresharma;
-
-create index idx_meta_deleted_at
-on public.meta (deleted_at);
-
-create table public.page_links
-(
-id         text not null
-primary key,
-created_at timestamp with time zone,
-updated_at timestamp with time zone,
-deleted_at timestamp with time zone,
-name       text,
-link       text,
-page_id    text
-constraint fk_pages_links
-references public.pages,
-icon       text,
-social     boolean,
-sequence   bigint
-);
-
-alter table public.page_links
-owner to iresharma;
-
-create index idx_page_links_deleted_at
-on public.page_links (deleted_at);
-
-create table public.maillist
-(
-id         serial
-primary key,
-email      varchar(255) not null,
-topics     varchar(255) not null,
-created_at timestamp    not null
-);
-
-alter table public.maillist
-owner to iresharma;
-
-create unique index maillist_email
-on public.maillist (email);
-
-create table public.settings
-(
-id         text not null
-primary key,
-created_at timestamp with time zone,
-updated_at timestamp with time zone,
-deleted_at timestamp with time zone
-);
-
-alter table public.settings
-owner to iresharma;
-
-create index idx_settings_deleted_at
-on public.settings (deleted_at);
-
-create table public.metadata
-(
-id         text not null
-primary key,
-created_at timestamp with time zone,
-updated_at timestamp with time zone,
-deleted_at timestamp with time zone,
-name       text,
-photo_url  text
-);
-
-alter table public.metadata
-owner to iresharma;
-
-create index idx_metadata_deleted_at
-on public.metadata (deleted_at);
+create index if not exists idx_metadata_deleted_at
+    on metadata (deleted_at);
 
